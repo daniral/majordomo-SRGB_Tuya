849fdba1e00d59f2b77d1b5bafaaef60b9477293
daniral <daniralgal@gmail.com>
2025-01-16 06:38:48 +0200

    fix

--- modules/devices/SRGB_Tuya_setBrightness.php
+++ modules/devices/SRGB_Tuya_setBrightness.php
@@ -2,13 +2,12 @@
 /*
 Установиь brightness в (array("value"=> 0--100))
 */
-$newValue;
-
 if (isset($params['value']) && is_numeric($params['value'])) {
-    if ($params['value'] < 0) $newValue = 0;
-    if ($params['value'] > 100) $newValue = 100;
-    if ($newValue > 0) $this->setProperty('flag', 1);
-    $this->setProperty('brightness', $newValue);
+	$newValue=$params['value'];
 } else {
     return;
 }
+if ($newValue < 0) $newValue = 0;
+if ($newValue > 100) $newValue = 100;
+if ($newValue > 0) $this->setProperty('flag', 1);
+$this->setProperty('brightness', $newValue);
